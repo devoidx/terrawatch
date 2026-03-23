c57d78848d3f8525c2e28c5c48460fb3f279327b
Tony McMahon <tony.mcmahon@gmail.com>
2026-03-23 08:58:47 +0000

    Use port 8001 for backend to avoid price-tracker clash

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,8 +4,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    port: 3002
     proxy: {
-      '/api': 'http://localhost:8000',
+      '/api': 'http://localhost:8001',
     },
   },
 })
